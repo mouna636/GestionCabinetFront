b3b408e9f847d722817d3b3ddff14c553354b195
Ben Hmida Aymen <118215572+AymenBH52@users.noreply.github.com>
2023-11-18 15:07:43 +0100

    ajout medicament

--- src/app/ajout-ordonnance/ajout-ordonnance.component.ts
+++ src/app/ajout-ordonnance/ajout-ordonnance.component.ts
@@ -1,87 +1,78 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { PatientService } from '../services/patient.service';
 import { Router } from '@angular/router';
-
-
 
 @Component({
   selector: 'app-ajout-ordonnance',
   templateUrl: './ajout-ordonnance.component.html',
-  styleUrls: ['./ajout-ordonnance.component.css']
+  styleUrls: ['./ajout-ordonnance.component.css'],
 })
 export class AjoutOrdonnanceComponent {
-
   addPatientForm: any;
   patient: any = {};
   addOrdonnanceTraitementForm: any;
   ordonnanceTraitement: any = {};
-  addMedicamentForm: any;
-  medicaments: any[] = [{ nom: '', posologie: '', nombreUnite: '', qsp: '' }]; 
+  medicamentForm!: FormGroup;
+  medicaments: FormArray;
   previewData: any = {};
   showPreview: boolean = false;
   hideOtherContent: boolean = false;
 
-    constructor( private formBuilder:FormBuilder,
-                 private patientService:PatientService,
-                 private router:Router) { }
-
-    
-    ngOnInit() {
-      this.addPatientForm = this.formBuilder.group({
-        nomPatient: [''],
-        prenomPatient: [''],
-        dateNaissance: [''],
-        sexe: [''],
-        adresse: [''],
-        situationFamilliale: [''],
-        assusranceMedical: [''],
-        codeAssurance: [''],
-        telephone: ['']
-      });
-
-      this.addOrdonnanceTraitementForm = this.formBuilder.group({
-       dateOrdonnance : [''],
-        
-      });
-      this.addMedicamentForm = this.formBuilder.group({
-        nom: [''],
-        posologie: [''],
-        nombreUnite: [''],
-        qsp: ['']
-      });
-
-    }
-
-     // Méthode pour ajouter un nouveau ligne de table pour les médicaments
-    addMedicament() {
-      this.medicaments.push({ nom: '', posologie: '', nombreUnite: '', qsp: '' });
+  constructor(
+    private formBuilder: FormBuilder,
+    private patientService: PatientService,
+    private router: Router
+  ) {
+    this.medicaments = this.formBuilder.array([this.createMedFormGroup()]);
   }
-   
-  // Méthode pour supprimer un ligne de table pour les médicaments
-  removeMedicament(medicament: any) {
-    const index = this.medicaments.indexOf(medicament);
-    if (index !== -1) {
-        this.medicaments.splice(index, 1);
+
+  ngOnInit() {
+    this.addPatientForm = this.formBuilder.group({
+      nomPatient: [''],
+      prenomPatient: [''],
+      dateNaissance: [''],
+      sexe: [''],
+      adresse: [''],
+      situationFamilliale: [''],
+      assusranceMedical: [''],
+      codeAssurance: [''],
+      telephone: [''],
+    });
+
+    this.addOrdonnanceTraitementForm = this.formBuilder.group({
+      dateOrdonnance: [''],
+    });
+
+    this.medicamentForm = this.formBuilder.group({
+      medicaments: this.formBuilder.array([this.createMedFormGroup()]),
+    });
+  }
+
+  addMedFormGroup() {
+    const medicaments = this.medicamentForm.get('medicaments') as FormArray;
+    medicaments.push(this.createMedFormGroup());
+  }
+
+  getControls() {
+    return (this.medicamentForm.get('medicaments') as FormArray).controls;
+  }
+
+  removeMedicament(i: number) {
+    const meds = this.medicamentForm.get('medicaments') as FormArray;
+    if (meds.length > 1) {
+      meds.removeAt(i);
+    } else {
+      meds.reset();
     }
+  }
+
+  createMedFormGroup(): FormGroup {
+    return new FormGroup({
+      nom: new FormControl(''),
+      posologie: new FormControl(''),
+      nombreUnite: new FormControl(''),
+      qsp: new FormControl(''),
+    });
+  }
 }
-//
-  /* printThisPage() {
-    this.previewData = {
-      patient: this.addPatientForm.value,
-      ordonnanceTraitement: this.addOrdonnanceTraitementForm.value,
-      medicaments: this.medicaments
-    };
-    this.showPreview = true;
-    this.hideOtherContent = true;
-    window.print();
-  } */
-  
-
-  
-}
-
-
-
-
-
